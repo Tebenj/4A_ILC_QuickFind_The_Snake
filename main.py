7745b0ec9e2b7e04b55815726aaae4d545587e7a
Federdispi <federdispi@gmail.com>
2023-01-13 09:29:32 +0100

    Updated main.py
    All the functions needed are now working perfectly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# echo build up
+
 from flask import Flask, request
-import json
-from collections import namedtuple
+# import json
+# from collections import namedtuple
 
 app = Flask(__name__)
 
@@ -10,33 +12,47 @@ class Transaction:
         self.P2 = P2
         self.t = t
         self.s = s
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    # def toJSON(self):
+    #     return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 class Personne:
     def __init__(self, solde, transactions):
         self.solde = solde
         self.transactions = transactions
-    def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+    # def toJSON(self):
+    #     return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
-def JSONDecoder(dict):
-    return namedtuple('X', dict.keys())(*dict.values())
+# def JSONDecoder(dict):
+#     return namedtuple('X', dict.keys())(*dict.values())
 
 transactions = []
 personnes = []
 
 @app.route('/')
+def listeTransactions():
+    transactions.sort(key=lambda tr: tr['t'])
+    return transactions
+
+@app.route('/personnes')
 def listPersonnes():
     return personnes
-def listeTransactions() :
-    return transactions
+
+@app.route('/personnes/transactions/<personne>')
+def transactionsPersonne(personne):
+    if request.method == 'GET':
+        return personnes[int(personne)]['transactions']
+
+@app.route('/personnes/solde/<personne>')
+def soldePersonne(personne):
+    if request.method == 'GET':
+        return personnes[int(personne)]['solde']
+
 
 @app.route('/addPersonne', methods=["POST"])
 def addPersonne():
     if request.method == 'POST':
         solde = request.form.get('solde')
-        personnes.append(Personne(solde, []).toJSON())
+        personnes.append({'solde': solde, 'transactions': []})
         return listPersonnes()
 
 @app.route('/addTransaction', methods=["POST"])
@@ -46,10 +62,8 @@ def addTransaction():
         P2_index = int(request.form.get('P2'))
         P1_data = personnes[P1_index]
         P2_data = personnes[P2_index]
-        P1_decoded = json.loads(P1_data, object_hook=JSONDecoder)
-        P2_decoded = json.loads(P2_data, object_hook=JSONDecoder)
-        P1 = Personne(P1_decoded.solde, P1_decoded.transactions)
-        P2 = Personne(P2_decoded.solde, P2_decoded.transactions)
+        P1 = Personne(P1_data['solde'], P1_data['transactions'])
+        P2 = Personne(P2_data['solde'], P2_data['transactions'])
         t = int(request.form.get('t'))
         s = int(request.form.get('s'))
         s1 = int(P1.solde)
@@ -58,10 +72,10 @@ def addTransaction():
         s2 += s
         P1.solde = str(s1)
         P2.solde = str(s2)
-        transaction = Transaction(P1, P2, t, s)
-        transactions.append(transaction.toJSON())
-        P1.transactions.append(transaction.toJSON())
-        P2.transactions.append(transaction.toJSON())
-        personnes[P1_index] = P1.toJSON()
-        personnes[P2_index] = P2.toJSON()
+        tr = {'P1': P1_index, 'P2': P2_index, 't': t, 's': s}
+        transactions.append(tr)
+        P1.transactions.append(tr)
+        P2.transactions.append(tr)
+        personnes[P1_index] = {'solde': P1.solde, 'transactions': P2.transactions}
+        personnes[P2_index] = {'solde': P2.solde, 'transactions': P2.transactions}
         return listeTransactions()
